b119220f46b078ed2ea0f3bc8b44d42f55abacf8
sebas-serrano <serranoavila@gmail.com>
2025-04-23 14:17:46 -0300

    contruimos el esqueleto del proyecto

--- src/main/java/uy/tilsor/backend/dto/ClienteDTO.java
+++ src/main/java/uy/tilsor/backend/dto/ClienteDTO.java
@@ -1,11 +1,36 @@
 package uy.tilsor.backend.dto;
 
+
 public class ClienteDTO {
-    
+
 	private Long id;
-    
+
 	private String nombre;
-    
+
 	private String email;
-    
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
--- src/main/java/uy/tilsor/backend/model/Cliente.java
+++ src/main/java/uy/tilsor/backend/model/Cliente.java
@@ -1,13 +1,43 @@
 package uy.tilsor.backend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nombre;
-    private String email;
-    // getters y setters
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
